79ad7316420a2bf499ad41f01f510427da55e9e3
parkjinhong03 <jinhong0719@naver.com>
2019-11-30 23:42:01 +0900

    [CREATE] Email/services.py -> 인증되지 않는 email QuerySet 삭제

--- repo/Email/services.py
+++ repo/Email/services.py
@@ -10,15 +10,27 @@ class EmailService(object):
 
     @staticmethod
     def check_email_auth_status(email):
-        print(1)
         return EmailAuth.objects.get(email=email).auth_status
 
     @staticmethod
-    def send_email(*email):
-        send_mail(
-            'Subject',
-            'Message',
-            'richimous0719@gmail.com',
-            email,
-            fail_silently=False,
-        )
+    def delete_email_if_exist(email):
+        if EmailService.check_email_exists(email):
+            EmailAuth.objects.get(email=email).delete()
+
+    @staticmethod
+    def send_email(*email_list, code):
+        for email in email_list:
+            send_mail(
+                'ChickSoup 이메일 인증 코드입니다.',
+                f"""
+                If you want to go through the certification process for our service, you need to read this article.
+
+                Hello, customer with e-mail {email}?
+                Thank you very much for your email certification to use our service.
+                The authentication number for your email we provided is {code}.
+                """,
+                'richimous0719@gmail.com',
+                [email],
+                fail_silently=False,
+            )
+
--- repo/Email/views.py
+++ repo/Email/views.py
@@ -17,9 +17,10 @@ class EmailCheckAPI(APIView):
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        data = serializer.initial_data
-        if EmailService.check_email_exists(data["email"]) and EmailService.check_email_auth_status(data["email"]):
+        email = serializer.initial_data["email"]
+        if EmailService.check_email_exists(email) and EmailService.check_email_auth_status(email):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        EmailService.send_email(data["email"])
-        return Response(data, status=status.HTTP_200_OK)
+        EmailService.delete_email_if_exist(email)
+        EmailService.send_email(email, code="asdfa323s")
+        return Response({"email": email}, status=status.HTTP_200_OK)
