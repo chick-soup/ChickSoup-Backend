2d9fa28498a59b4dc953fc6c2cac62f49c8b29f8
parkjinhong03 <jinhong0719@naver.com>
2019-11-30 16:11:53 +0900

    [CREATE] Email/views.py -> '클래스형 뷰 생성'

--- repo/Email/views.py
+++ repo/Email/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class EmailCheckAPI(APIView):
+    def get(self, request):
+        return Response("hello world!", status=status.HTTP_200_OK)
