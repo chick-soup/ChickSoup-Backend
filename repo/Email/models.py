6c82ba3ea4adc5776396214df5a59b149d974f0e
parkjinhong03 <jinhong0719@naver.com>
2019-11-30 16:59:06 +0900

    [UPDATE] Email/models.py -> verbose_name_plural 설정

--- repo/Email/models.py
+++ repo/Email/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class EmailAuth(models.Model):
+    class Meta:
+        verbose_name_plural = 'EmailAuth'
     email = models.CharField(max_length=30)
     auth_code = models.CharField(max_length=10)
     auth_status = models.BooleanField()
