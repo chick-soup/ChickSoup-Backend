67fff266e5fa2dc64cb6511bd2b084b998456620
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 22:12:52 +0900

    [ADD] User/services.py -> hashed password 생성 메서드 추가

--- repo/User/services.py
+++ repo/User/services.py
@@ -1,3 +1,5 @@
+import bcrypt
+
 from .models import User
 
 
@@ -5,3 +7,9 @@ class UserService(object):
     @staticmethod
     def check_email_exists(email: str) -> bool:
         return True if len(User.objects.filter(email=email).values()) else False
+
+
+class HashService(object):
+    @staticmethod
+    def hash_string_to_password(password: str) -> str:
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
