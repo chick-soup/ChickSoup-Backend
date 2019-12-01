1e0e0b111607ea3c304d7203c9b5e68d1b5ecc37
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 12:59:55 +0900

    [CREATE] Email/exception.py -> 인증 요청X 이메일 exception 처리

--- repo/Email/exceptions.py
+++ repo/Email/exceptions.py
@@ -4,3 +4,8 @@ from rest_framework.exceptions import APIException
 class EmailExists(APIException):
     status_code = 470
     default_detail = "This email is already used."
+
+
+class EmailNotRequestAuth(APIException):
+    status_code = 471
+    default_detail = "This email has not been requested for authentication"
