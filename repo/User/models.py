59ecdd51e72895710cdc3ce027c139103c47b345
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 14:14:01 +0900

    [CREATE] -> User/models.py -> User & UserInform 테이블 추가

--- repo/User/models.py
+++ repo/User/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=30)
+    password = models.TextField()
+
+    def __str__(self):
+        return self.email
+
+
+class UserInform(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=12)
+    status_message = models.CharField(max_length=100)
