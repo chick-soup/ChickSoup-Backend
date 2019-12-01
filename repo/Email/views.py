f2b8241fdc96713bfa93aa1c9289428d695d17d8
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 13:17:14 +0900

    [UPDATE] Email/views.py -> /email/auth의 모든 로직 적용 완료

--- repo/Email/views.py
+++ repo/Email/views.py
@@ -3,15 +3,40 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import (
-    EmailCheckSerializers
+    EmailCheckSerializers,
+    EmailAuthSerializers
 )
 from .services import (
     EmailService,
     Random,
 )
 from .exceptions import (
-    EmailExists
+    EmailExists,
+    EmailNotRequestAuth,
+    AuthCodeDoseNotMatch
 )
+
+
+class EmailAuthAPI(APIView):
+    def post(self, request):
+        serializer = EmailAuthSerializers(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        data = serializer.data
+        email = data["email"]
+        if not EmailService.check_email_exists(email):
+            raise EmailNotRequestAuth
+
+        if EmailService.check_email_exists(email) and EmailService.check_email_auth_status(email):
+            raise EmailExists
+
+        if data["auth_code"] != EmailService.check_email_auth_code(email):
+            raise AuthCodeDoseNotMatch
+
+        EmailService.email_auth_complete(email)
+        return Response(data, status=status.HTTP_200_OK)
 
 
 class EmailCheckAPI(APIView):
