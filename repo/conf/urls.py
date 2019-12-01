f001fba3e2aa6630aca1e220777f7081bf348d64
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 14:26:28 +0900

    [CREATE] conf/urls.py -> urls 최상위 폴더에 'user/' 라우팅

--- repo/conf/urls.py
+++ repo/conf/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('email/', include('Email.urls'))
+    path('email/', include('Email.urls')),
+    path('user/', include('User.urls')),
 ]
