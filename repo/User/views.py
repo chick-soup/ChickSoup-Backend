f52770640bf47711f64d69455d1d7fdb1d322bb8
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 21:05:07 +0900

    [ADD] User/views.py -> SignUpAPI 생성 테스트

--- repo/User/views.py
+++ repo/User/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class SignUpAPI(APIView):
+    def post(self, request):
+        return Response("hello world!", status=status.HTTP_200_OK)
