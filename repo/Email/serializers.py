1a72dae2bf763b89f745c4d5c9fcc3801c7584df
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 12:37:30 +0900

    [CREATE] Email/serializers.py -> EmailAuthSerializers 생성

--- repo/Email/serializers.py
+++ repo/Email/serializers.py
@@ -4,3 +4,8 @@ from .models import EmailAuth
 
 class EmailCheckSerializers(serializers.Serializer):
     email = serializers.CharField(max_length=30)
+
+
+class EmailAuthSerializers(serializers.Serializer):
+    email = serializers.CharField(max_length=30)
+    auth_code = serializers.CharField()
