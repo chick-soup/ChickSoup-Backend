fe488fe512923a6f16f02c28434885247215958d
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 22:19:42 +0900

    User/services.py -> 새 사용자 데이터 생성 메서드 추가

--- repo/User/services.py
+++ repo/User/services.py
@@ -8,8 +8,13 @@ class UserService(object):
     def check_email_exists(email: str) -> bool:
         return True if len(User.objects.filter(email=email).values()) else False
 
+    @staticmethod
+    def create_new_user(email: str, hashed_password: str) -> None:
+        User(email=email, password=hashed_password).save()
+
 
 class HashService(object):
     @staticmethod
     def hash_string_to_password(password: str) -> str:
         return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
