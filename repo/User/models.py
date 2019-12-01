6dd1be49c2cd72e2bad72c49fb4d3e6aaa6c3bf1
parkjinhong03 <jinhong0719@naver.com>
2019-12-01 21:55:47 +0900

    [ADD] User/models.py -> verbose_name_plural 설정

--- repo/User/models.py
+++ repo/User/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class User(models.Model):
+    class Meta:
+        verbose_name_plural = 'User'
     id = models.AutoField(primary_key=True)
     email = models.CharField(max_length=30)
     password = models.TextField()
@@ -11,6 +13,8 @@ class User(models.Model):
 
 
 class UserInform(models.Model):
+    class Meta:
+        verbose_name_plural = "UserInform"
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=12)
     status_message = models.CharField(max_length=100)
