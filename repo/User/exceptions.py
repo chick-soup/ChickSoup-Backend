d2e5281a1da7fc83d2fa93547ea55e28881e7efa
민기남친지농이 <jinhong0719@naver.com>
2019-12-03 21:57:27 +0900

    [ADD] <User/exceptions.py> 해당 pk의 사용자가 존재하지 않을 경우 exception 발생

--- repo/User/exceptions.py
+++ repo/User/exceptions.py
@@ -6,6 +6,11 @@ class UnauthenticatedEmail(APIException):
     default_detail = "This email is not validated."
 
 
+class UserNotFound(APIException):
+    status_code = 404
+    default_detail = "user who has this primary key is not exist."
+
+
 class ExpiredJWT(APIException):
     status_code = 403
     default_detail = "This JWT is expired."
