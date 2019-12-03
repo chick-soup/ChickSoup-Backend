53dcab8199abdd00a333682b8b7729881f8de18c
Maly <migsking@naver.com>
2019-12-03 22:09:01 +0900

    [MODIFY] 엥 추가하는데 북마크가 왜 필요함

--- repo/Friend/views.py
+++ repo/Friend/views.py
@@ -2,8 +2,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from Friend.serializers import RegisterFriendSerializers
+
 
 class RegisterFriendAPI(APIView):
     def post(self, request):
+        serializer = RegisterFriendSerializers(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        data = serializer.initial_data
+
+        guest_id = data['guest_id']
+
+        
 
         return Response("TEST", 200)
