85da210fb06b9bd2fd94fb3d97f4e25827310ff9
민기남친지농이 <jinhong0719@naver.com>
2019-12-03 13:11:29 +0900

    [ADD] <User/exceptions.py> JWT 관련 exceptions 추가

--- repo/User/exceptions.py
+++ repo/User/exceptions.py
@@ -4,3 +4,19 @@ from rest_framework.exceptions import APIException
 class UnauthenticatedEmail(APIException):
     status_code = 471
     default_detail = "This email is not validated."
+
+
+class ExpiredJWT(APIException):
+    status_code = 403
+    default_detail = "This JWT is expired."
+
+
+class NoIncludeJWT(APIException):
+    status_code = 401
+    default_detail = "You should include JWT in headers with Authorization."
+
+
+class IncorrectJWT(APIException):
+    status_code = 422
+    default_detail = "We can't decode this JWT."
+
