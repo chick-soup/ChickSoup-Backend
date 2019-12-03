8cecaeebf3f6ee7b3ed5138f3ba31021bba8d428
민기남친지농이 <jinhong0719@naver.com>
2019-12-04 08:28:33 +0900

    [ADD] <User/serializers.py> '/login'용 serializers 추가

--- repo/User/serializers.py
+++ repo/User/serializers.py
@@ -10,3 +10,7 @@ class ProfileSerializers(serializers.Serializer):
     nickname = serializers.CharField(max_length=12)
     status_message = serializers.CharField(max_length=100)
 
+
+class LoginSerializers(serializers.Serializer):
+    email = serializers.CharField(max_length=30)
+    password = serializers.CharField()
