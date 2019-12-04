24745f06f6304ff00a3f16a439bb21d44e261f68
민기남친지농이 <jinhong0719@naver.com>
2019-12-04 17:59:42 +0900

    [MODIFY]<conf/settings.py> MIDDLEWARE에 CORS 적용

--- repo/conf/settings.py
+++ repo/conf/settings.py
@@ -51,12 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-MIDDLEWARE_CLASSES = (
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-)
+]
 
 ROOT_URLCONF = 'conf.urls'
 
