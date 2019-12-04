2be48ddd6207b65d868a1073dbab9cfe7c22d62c
민기남친지농이 <jinhong0719@naver.com>
2019-12-04 19:46:21 +0900

    [MODIFY] <EmailAuth> EmailAuth DB에 auth_ip 컬럼 추가

--- repo/Email/models.py
+++ repo/Email/models.py
@@ -7,6 +7,7 @@ class EmailAuth(models.Model):
     email = models.CharField(max_length=30)
     auth_code = models.CharField(max_length=10)
     auth_status = models.BooleanField()
+    auth_ip = models.CharField(max_length=20, default="127.0.0.1")
 
     def __str__(self):
         return self.email
