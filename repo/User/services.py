9051afbdb112d93b55c943ca57974a161e2dfcf5
민기남친지농이 <jinhong0719@naver.com>
2019-12-05 21:46:12 +0900

    [MODIFY] <User.views.py> 프로필 업데이트 메서드에서 상태 메세지 기본값 None

--- repo/User/services.py
+++ repo/User/services.py
@@ -38,7 +38,7 @@ class UserService(object):
         return user.id
 
     @staticmethod
-    def update_user_profile(pk: int, nickname: str, status_message: str) -> None:
+    def update_user_profile(pk: int, nickname: str, status_message: str = None) -> None:
         user = UserInform.objects.get(user_id=User.objects.get(pk=pk))
         user.nickname = nickname
         user.status_message = status_message
