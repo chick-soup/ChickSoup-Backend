8b58560e25bfdc07d63cecd28e970f278dbe3e61
민기남친지농이 <jinhong0719@naver.com>
2019-12-05 23:41:10 +0900

    [MODIFY] <conf/settings.py> AWS RDS PostgreSQL 연동

--- repo/Email/models.py
+++ repo/Email/models.py
@@ -5,7 +5,7 @@ class EmailAuth(models.Model):
     class Meta:
         verbose_name_plural = 'EmailAuth'
     email = models.CharField(max_length=30)
-    auth_code = models.CharField(max_length=10)
+    auth_code = models.CharField(max_length=20)
     auth_status = models.BooleanField()
     auth_ip = models.CharField(max_length=20, default=None, null=True)
 
