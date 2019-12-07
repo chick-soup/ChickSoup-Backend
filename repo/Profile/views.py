f547891a1b503156038c8922b96d1f381053d165
민기남친지농이 <jinhong0719@naver.com>
2019-12-07 23:32:51 +0900

    [CREATE] <Profile/views.py> profile/<user_id> API 구현 완료

--- repo/Profile/views.py
+++ repo/Profile/views.py
@@ -19,10 +19,30 @@ class MyProfileAPI(APIView):
         if not UserService.check_pk_exists(pk):
             raise UserNotFound
 
-        my_profile = ProfileService.get_profile_with_pk(pk)
+        profile = ProfileService.get_profile_with_pk(pk)
 
         return Response({
             "id": pk,
-            "nickname": my_profile.nickname,
-            "status_message": "" if my_profile.status_message is None else my_profile.status_message
+            "nickname": profile.nickname,
+            "status_message": "" if profile.status_message is None else profile.status_message
         }, status=status.HTTP_200_OK)
+
+
+class PkProfileAPI(APIView):
+    def get(self, request, user_id):
+        pk = JWTService.run_auth_process(request.headers)
+
+        if not (UserService.check_pk_exists(pk) and UserService.check_pk_exists(user_id)):
+            raise UserNotFound
+
+        profile = ProfileService.get_profile_with_pk(user_id)
+
+        # 친구 API 구현 후 해당 user_id가 친구가 아니면 exception 추가 하기
+
+        return Response({
+            "id": user_id,
+            "nickname": profile.nickname,
+            "status_message": "" if profile.status_message is None else profile.status_message,
+            "myself": True if pk is user_id else False
+        }, status=status.HTTP_200_OK)
+
