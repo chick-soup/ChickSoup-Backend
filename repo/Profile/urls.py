43f744614786ee348794e545c097bc3befdf79e2
민기남친지농이 <jinhong0719@naver.com>
2019-12-07 23:31:43 +0900

    [ADD] <Profile/urls.py> profile/<user_id> 라우팅 추가

--- repo/Profile/urls.py
+++ repo/Profile/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import MyProfileAPI
-
+from .views import (
+    MyProfileAPI,
+    PkProfileAPI
+)
 urlpatterns = [
     path('my', MyProfileAPI.as_view()),
+    path("<int:user_id>", PkProfileAPI.as_view()),
 ]
