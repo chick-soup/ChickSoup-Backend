9105538c6dd95e4493cacdea691867055bff7c82
민기남친지농이 <jinhong0719@naver.com>
2019-12-07 22:24:14 +0900

    [CREATE] <Profile/views.py> MyProfileAPI Class 생성 및 인증 추가

--- repo/Profile/views.py
+++ repo/Profile/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from User.services import JWTService
+
+
+class MyProfileAPI(APIView):
+    def get(self, request):
+        pk = JWTService.run_auth_process(request.headers)
+
+        return Response({"id": pk}, status=status.HTTP_200_OK)
