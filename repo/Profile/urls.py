a17a480bcff788fcd15c485d7cf78e43b885cf05
민기남친지농이 <jinhong0719@naver.com>
2019-12-08 22:27:26 +0900

    [MODIFY] <urls.py> 프로필 조회 API에서 URL의 /profile 삭제

--- repo/Profile/urls.py
+++ repo/Profile/urls.py
@@ -9,5 +9,5 @@ from Kakao.views import MyKakaoIdAPI
 urlpatterns = [
     path('my/profile', MyProfileAPI.as_view()),
     path('my/kakao-id', MyKakaoIdAPI.as_view()),
-    path("<int:user_id>/profile", PkProfileAPI.as_view()),
+    path("<int:user_id>", PkProfileAPI.as_view()),
 ]
