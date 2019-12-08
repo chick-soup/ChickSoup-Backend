2f6a4e59a4fd4db82dd1d12c7a397b8ccfaa89fc
민기남친지농이 <jinhong0719@naver.com>
2019-12-08 14:37:53 +0900

    [CREATE] <Kakao/views.py> 내 카카오 아이디 반환 API 구현 완료

--- repo/Kakao/views.py
+++ repo/Kakao/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+from User.services import (
+    JWTService,
+    UserService
+)
+from User.exceptions import UserNotFound
+from .services import KakaoIdService
+
+
+class MyKakaoIdAPI(APIView):
+    def get(self, request):
+        pk = JWTService.run_auth_process(request.headers)
+
+        if not UserService.check_pk_exists(pk):
+            raise UserNotFound
+
+        return Response({"kakao_id": KakaoIdService.check_kakao_id_with_pk(pk)}, status=status.HTTP_200_OK)
