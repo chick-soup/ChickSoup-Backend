9f4a8a2d276e139952694d4ef2760c87173b64f1
민기남친지농이 <jinhong0719@naver.com>
2019-12-08 11:31:44 +0900

    [ADD] <Profile/services.py> pk 사용자의 프로필 정보 변경 메서드

--- repo/Profile/services.py
+++ repo/Profile/services.py
@@ -6,3 +6,10 @@ class ProfileService(object):
     @staticmethod
     def get_profile_with_pk(pk: int) -> UserInform:
         return UserInform.objects.get(user_id=UserService.get_user_by_pk(pk))
+
+    @staticmethod
+    def change_profile_with_pk(pk: int, nickname: str, status_message: str) -> None:
+        profile = UserInform.objects.get(user_id=UserService.get_user_by_pk(pk))
+        profile.nickname = nickname
+        profile.status_message = status_message
+        profile.save()
