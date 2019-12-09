0a59594d87f749939e067ab20ae63fb3e46a15d6
지농 <jinhong0719@naver.com>
2019-12-09 20:25:55 +0900

    [REMOVE] <Friend/views.py> kakao-id/<kakao_id> POST DELETE API 삭제

--- repo/Friend/views.py
+++ repo/Friend/views.py
@@ -21,47 +21,6 @@ from Kakao.services import (
 )
 
 
-class KakaoIdFriendAPI(object):
-    @staticmethod
-    def post(request, kakao_id):
-        host_id = JWTService.run_auth_process(request.headers)
-
-        if not KakaoIdService.check_kakao_id_exist(kakao_id):
-            raise FriendNotFound
-
-        guest_id = KakaoIdService.get_pk_with_kakao_id(kakao_id)
-
-        if host_id == guest_id:
-            raise Myself
-        if FriendService.check_if_friend_or_not(host_id=host_id, guest_id=guest_id):
-            if FriendService.check_if_friend_or_not(host_id=guest_id, guest_id=host_id):
-                raise AlreadyFriend
-            raise AlreadyRequest
-
-        FriendService.create_new_friend(host_id=host_id, guest_id=guest_id)
-
-        if FriendService.check_if_friend_or_not(host_id=guest_id, guest_id=host_id):
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_200_OK)
-
-    @staticmethod
-    def delete(request, kakao_id):
-        host_id = JWTService.run_auth_process(request.headers)
-
-        if not KakaoIdService.check_kakao_id_exist(kakao_id):
-            raise FriendNotFound
-
-        guest_id = KakaoIdService.get_pk_with_kakao_id(kakao_id)
-
-        if host_id == guest_id:
-            raise Myself
-        if not FriendService.check_both_friend(host_id, guest_id):
-            raise NotFriend
-
-        FriendService.delete_friend(host_id, guest_id)
-        return Response(status=status.HTTP_200_OK)
-
-
 class UserIdFriendAPI(object):
     @staticmethod
     def post(request, guest_id):
