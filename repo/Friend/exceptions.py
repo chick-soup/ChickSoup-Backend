fae1ed853c5e239144cdd0dc5ef006c5e5207b28
지농 <jinhong0719@naver.com>
2019-12-09 09:30:31 +0900

    [CREATE] <Friend/exceptions.py> 이미 친구 요청을 보냈을 경우의 exception 추가

--- repo/Friend/exceptions.py
+++ repo/Friend/exceptions.py
@@ -4,3 +4,8 @@ from rest_framework.exceptions import APIException
 class FriendNotFound(APIException):
     status_code = 470
     default_detail = "The user for this friend_id is not exist"
+
+
+class AlreadyFriend(APIException):
+    status_code = 471
+    default_detail = "This is the user who has already sent the friend request."
