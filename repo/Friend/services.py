43291445a5705ffb53843b5e1b90dc7f2a78aeb7
지농 <jinhong0719@naver.com>
2019-12-10 17:22:20 +0900

    [ADD] <Friend/services.py> 해당 친구와의 상태 변경 메서드 추가

--- repo/Friend/services.py
+++ repo/Friend/services.py
@@ -22,11 +22,33 @@ class FriendService(object):
         Friend.objects.get(host_id=guest_id, guest_id=host_id).delete()
 
     @staticmethod
+    def set_friend_status(host_id: int, guest_id: int, key: str, status: bool) -> None:
+        friend = Friend.objects.get(host_id=host_id, guest_id=guest_id)
+        if key is 'mute':
+            friend.mute = status
+        if key is 'hidden':
+            friend.hidden = status
+        if key is 'bookmark':
+            friend.bookmark = status
+        friend.save()
+
+
+class UserPutAPIService(object):
+    @staticmethod
     def check_put_bad_request(data: dict) -> bool:
         count = 0
         for key in ['mute', 'hidden', 'bookmark']:
-            if key in data:
+            if key in data and (data[key] is '0' or data[key] is '1'):
                 count += 1
         if count is 1:
             return False
         return True
+
+    @staticmethod
+    def get_key_from_data(data: dict) -> str:
+        if 'mute' in data:
+            return 'mute'
+        if 'hidden' in data:
+            return 'hidden'
+        if 'bookmark' in data:
+            return 'bookmark'
