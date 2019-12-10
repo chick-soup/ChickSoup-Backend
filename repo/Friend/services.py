8bcef9910c5cc9a863a636136d458a7a9086850f
지농 <jinhong0719@naver.com>
2019-12-10 16:47:53 +0900

    [ADD] <Friend/services.py> users/<user_id> PUT API 400 로직 추가

--- repo/Friend/services.py
+++ repo/Friend/services.py
@@ -20,3 +20,13 @@ class FriendService(object):
     def delete_friend(host_id: int, guest_id: int) -> None:
         Friend.objects.get(host_id=host_id, guest_id=guest_id).delete()
         Friend.objects.get(host_id=guest_id, guest_id=host_id).delete()
+
+    @staticmethod
+    def check_put_bad_request(data: dict) -> bool:
+        count = 0
+        for key in ['mute', 'hidden', 'bookmark']:
+            if key in data:
+                count += 1
+        if count is 1:
+            return False
+        return True
