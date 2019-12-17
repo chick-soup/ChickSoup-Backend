3d3c17974b3f9929d1a77e08855ade4f86b9d60e
지농 <jinhong0719@naver.com>
2019-12-17 22:33:05 +0900

    [MODIFY] <Profile/serializers.py> status_massage allow_null=True

--- repo/Profile/serializers.py
+++ repo/Profile/serializers.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 
 class MyProfilePutSerializer(serializers.Serializer):
     nickname = serializers.CharField(max_length=12)
-    status_message = serializers.CharField(max_length=100)
+    status_message = serializers.CharField(max_length=100, allow_null=True)
     where = serializers.CharField(max_length=10)
